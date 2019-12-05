6d6d4b105aeef3ea5a871c3882cc48b30c3a8f1c
rcdaflon <rcdaflon@outlook.com>
2019-12-05 17:10:39 -0300

    update

--- VeiculosOnline-CLI/src/main/java/br/veiculosonline/client/controller/login/SigninFormController.java
+++ VeiculosOnline-CLI/src/main/java/br/veiculosonline/client/controller/login/SigninFormController.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.PostMapping;
  *
  * @author Rodrigo
  */
+@Controller
 public class SigninFormController {
     
     @Autowired
